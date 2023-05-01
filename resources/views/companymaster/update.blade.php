0bf931c3b8dd54c8d09467da5e1ab32393defcac
Ashishkumar6387784995 <485kumarashish@gmail.com>
2023-05-01 22:15:11 +0530

    changes

--- resources/views/companymaster/update.blade.php
+++ resources/views/companymaster/update.blade.php
@@ -226,7 +226,7 @@ input:focus, textarea:focus, select:focus{
                           <div class=" col-sm-12 col-md-3" style="width: 25%;">
                             <label style="height: 25px;"></label>
                             <div>
-                              <img src="/asset/images/btn_loader.gif" id="po_search_loader1" class="input_loader po_search_loader" style="display: none; position: unset;width: 25px;height: 25px;text-align: left;float: left;margin-left: -20px;margin-right: 10px;margin-top: 3px;">
+                              <img src="{{URL::asset('public/asset/images/btn_loader.gif')}}" id="po_search_loader1" class="input_loader po_search_loader" style="display: none; position: unset;width: 25px;height: 25px;text-align: left;float: left;margin-left: -20px;margin-right: 10px;margin-top: 3px;">
                               <button class="btn waves-effect waves-light left" type="button" onclick="updateData()" name="action" id="add_hotel" style="margin-right: 10px;height: 26px;padding: 4px 3px;background-color: #127623;font-size: 12px;line-height: 1;">Update</button>
                             <a href="{{URL::to('/company-master')}}" class="btn waves-effect waves-dark " style="background-color: #bfb32b;color: #fff;height: 26px;padding: 6px 4px;font-size: 12px;line-height: 1;">Back</a>
                           </div>
